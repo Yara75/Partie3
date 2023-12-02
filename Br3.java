fdda2c69d15a1454b6ea6d086ae078bf632ffa99
Dounia Safraoui <116631446+Yara75@users.noreply.github.com>
2023-12-02 17:32:15 +0100

    Update Br3.java

--- Br3.java
+++ Br3.java
@@ -1,7 +1,16 @@
 
 public class Br3{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello, World!");
-	}
+	public static void main(String[] args) {
+        System.out.println("Hello it's the main branch");
+
+        String[] devoirs = {"TP DEVOPS", "TP RESEAUX", "TP OS", "RAPPORT ALTERNANCE", "PPT ALTERNANCE", "TOEIC BLANC"};
+        List<String> taches_a_effectuer = new ArrayList<>();
+
+        for (String devoir : devoirs) {
+            taches_a_effectuer.add(devoir);
+        }
+	
+	String modifierNomTache = "TP OS 5";    
+	taches_a_effectuer.set(3, modifierNomTache);   
+    }
 }
