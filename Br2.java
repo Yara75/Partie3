f04d69684b48991d532dc4f0ecd29038d56d62fc
Dounia Safraoui <116631446+Yara75@users.noreply.github.com>
2023-12-02 17:31:02 +0100

    Update Br2.java

--- Br2.java
+++ Br2.java
@@ -1,7 +1,21 @@
 
-public class Br2{
-	public static void main(String[] args)
-	{
-		System.out.println("Hello, World!");
-	}
+import java.util.ArrayList;
+import java.util.List;
+
+public class Br2 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello it's the main branch");
+
+        String[] devoirs = {"TP DEVOPS", "TP RESEAUX", "TP OS", "RAPPORT ALTERNANCE", "PPT ALTERNANCE", "TOEIC BLANC"};
+        List<String> taches_a_effectuer = new ArrayList<>();
+
+        for (String devoir : devoirs) {
+            taches_a_effectuer.add(devoir);
+        }
+	
+	String fait = "TOEIC BLANC";    
+	taches_a_effectuer.remove(fait);    
+    }
 }
+
