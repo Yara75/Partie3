ae61f02a55ab9a6aa42564b2d60141ab0ad16dff
Dounia Safraoui <116631446+Yara75@users.noreply.github.com>
2023-12-02 17:36:34 +0100

    Update Br2.java

--- Br2.java
+++ Br2.java
@@ -5,7 +5,7 @@ import java.util.List;
 public class Br2 {
 
     public static void main(String[] args) {
-        System.out.println("Hello it's the main branch");
+        System.out.println("Hello it's the second branch");
 
         String[] devoirs = {"TP DEVOPS", "TP RESEAUX", "TP OS", "RAPPORT ALTERNANCE", "PPT ALTERNANCE", "TOEIC BLANC"};
         List<String> taches_a_effectuer = new ArrayList<>();
