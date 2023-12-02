ca7aaeb0d64beb68049ed5a61dfcfe83a5260ad4
Dounia Safraoui <116631446+Yara75@users.noreply.github.com>
2023-12-02 16:40:47 +0100

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,6 +2,13 @@
 public class Main{
 	public static void main(String[] args)
 	{
-		System.out.println("Hello, World!");
+		System.out.println("Hello it's the main branch");
+		int test = 5, test2 = 53;
+
+		if((test + test2) == 58)
+		{
+			System.out.println("Test Ok");
+		}
+		
 	}
 }
